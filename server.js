182d021a889cb851a548b802912e6bf029c56413
Glen Alloy <glenalloy8@gmail.com>
2024-07-23 12:28:16 +0530

    created users object array

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res) => {
     res.send('this is working');
+});
+
+app.post('/signin', (req,res) => {
+    res.json('signing');
 });
 
 app.listen(3000, () => {
