26cea505be3e2b91706d74dc55ab267b42ba744b
Glen Alloy <glenalloy8@gmail.com>
2024-08-07 12:31:38 +0530

    get profile endpoint implemented

--- server.js
+++ server.js
@@ -71,16 +71,16 @@ app.post('/register', (req,res) => {
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
-        }
-    });
-    if(!found){
-        res.status(400).json('Not Found');
-    }
+    db.select('*').from('users').where({id})
+        .then(user => {
+        console.log(user)
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json('Not found')
+        } 
+    })
+    .catch(err => res.status(400).json('error getting user'))
 });
 
 app.put('/image', (req,res) => {
