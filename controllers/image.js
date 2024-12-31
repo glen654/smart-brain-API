25467d45bf0c3bf42cd515385c165e86fee0d5e2
glen654 <glenalloy@gmail.com>
2024-12-31 19:44:18 +0530

    formatted project

--- controllers/image.js
+++ controllers/image.js
@@ -1,71 +1,74 @@
-const returnClarifaiRequest =(imageUrl) =>{
-    const PAT = 'a1f9cfd6934d4a279577e28985f79d77';
-    const USER_ID = '82nxn1prhu8q';       
-    const APP_ID = 'my-first-application-e38qz';
-    const IMAGE_URL = imageUrl;
-  
-    const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": IMAGE_URL
-                  }
-              }
-          }
-      ]
-  });
-  
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
+const returnClarifaiRequest = (imageUrl) => {
+  const PAT = "a1f9cfd6934d4a279577e28985f79d77";
+  const USER_ID = "82nxn1prhu8q";
+  const APP_ID = "my-first-application-e38qz";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
     },
-    body: raw
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
   };
   return requestOptions;
+};
+
+const handleApiCalls = (req, res) => {
+  const { input } = req.body;
+  if (!input) {
+    return res.status(400).json("No input provided");
   }
 
-  const handleApiCalls = (req,res) => {
-    const { input } = req.body; 
-    if (!input) {
-      return res.status(400).json('No input provided');
-    }
-  
-    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequest(input))
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        console.error('Error fetching from Clarifai API:', err);
-        res.status(400).json('Unable to work with API');
-      });
-  }
-
-
-const handleImage = (req,res,db) => {
-    const {id} = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
+  fetch(
+    "https://api.clarifai.com/v2/models/face-detection/outputs",
+    returnClarifaiRequest(input)
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      return response.json();
     })
-    .catch(err => res.status(400).json('unable to get entries count'))
-}
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.error("Error fetching from Clarifai API:", err);
+      res.status(400).json("Unable to work with API");
+    });
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries count"));
+};
 
 module.exports = {
-    handleImage,
-    handleApiCalls
-}
+  handleImage,
+  handleApiCalls,
+};
--- server.js
+++ server.js
@@ -1,49 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'postgres',
-      password: '1234',
-      database: 'smart-brain'
-    },
-  });
-
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "1234",
+    database: "smart-brain",
+  },
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-app.get('/', (req, res) => {
-    res.send('success');
+app.get("/", (req, res) => {
+  res.send("success");
 });
 
-app.post('/register', (req,res) => {register.handleRegister (req,res,db,bcrypt)});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
-app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)});
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
 
-app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
 
-app.put('/image', (req,res) => {image.handleImage(req,res,db)});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
 
-app.post('/imageUrl', (req, res) => { image.handleApiCalls(req, res)})
-
+app.post("/imageUrl", (req, res) => {
+  image.handleApiCalls(req, res);
+});
 
 app.listen(3000, () => {
-    console.log('app is runnig on port 3000');
+  console.log("app is runnig on port 3000");
 });
 
 /*
